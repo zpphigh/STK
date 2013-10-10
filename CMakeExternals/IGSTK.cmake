952d5703b172adb730167194ae5994e67961d96e
Weiming Zhai <wmzhai@gmail.com>
2013-10-10 19:30:41 +0800

    fix IGSTK build error in CMakeExternals/IGSTK.cmake

--- CMakeExternals/IGSTK.cmake
+++ CMakeExternals/IGSTK.cmake
@@ -45,19 +45,18 @@ if(${add_project})
           "-DCMAKE_PROJECT_IGSTK_INCLUDE:FILEPATH=${CMAKE_ROOT}/Modules/CTestUseLaunchers.cmake")
       endif()
 
-      #message(STATUS "Adding project:${proj}")
       ExternalProject_Add(${proj}
         SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
         BINARY_DIR ${proj}-build
         PREFIX ${proj}${ep_suffix}
         ${location_args}
-        CMAKE_GENERATOR ${gen}
 	    DOWNLOAD_COMMAND ""
         UPDATE_COMMAND ""
-        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+        CMAKE_GENERATOR ${gen}
         CMAKE_ARGS
-          -DIGSTK_INSTALL_BINDIR:STRING=bin/${CMAKE_CFG_INTDIR}
-          -DIGSTK_INSTALL_LIBDIR:STRING=lib/${CMAKE_CFG_INTDIR}
+          #-DIGSTK_INSTALL_BINDIR:STRING=bin/${CMAKE_CFG_INTDIR}
+          #-DIGSTK_INSTALL_LIBDIR:STRING=lib/${CMAKE_CFG_INTDIR}
         CMAKE_CACHE_ARGS
           ${ep_common_cache_args}
           ${ep_project_include_arg}
@@ -65,23 +64,7 @@ if(${add_project})
           -DIGSTK_USE_OpenIGTLink:BOOL=ON
           -DIGSTK_USE_Qt:BOOL=ON 
         )
-      set(IGSTK_DIR ${ep_install_dir})
-
-  # This was used during heavy development on IGSTK itself.
-  # Disabling it for now. (It also leads to to build errors
-  # with the XCode CMake generator on Mac).
-  #
-  #    ExternalProject_Add_Step(${proj} force_rebuild
-  #      COMMENT "Force ${proj} re-build"
-  #      DEPENDERS build    # Steps that depend on this step
-  #      ALWAYS 1
-  #      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/${proj}-build
-  #      DEPENDS
-  #        ${proj_DEPENDENCIES}
-  #      )
-
-      # Since IGSTK is statically build, there is not need to add its corresponding
-      # library output directory to STK_EXTERNAL_LIBRARY_DIRS
+      set(IGSTK_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
 
     else()
       stkMacroEmptyExternalproject(${proj} "${proj_DEPENDENCIES}")
