c2938b5b05069c989609cdb30ef07fe7c60d5c58
Weiming Zhai <wmzhai@gmail.com>
2013-10-27 17:44:39 +0800

    successfully add fiducial markers

--- Libs/SurgeryNavigation/stkFiducialMarkerRegistrationWidget.cpp
+++ Libs/SurgeryNavigation/stkFiducialMarkerRegistrationWidget.cpp
@@ -1,6 +1,10 @@
 #include "stkFiducialMarkerRegistrationWidget.h"
 #include "ui_stkFiducialMarkerRegistrationWidget.h"
+
 #include "qSlicerApplication.h"
+#include "vtkMRMLScene.h"
+#include "vtkMRMLInteractionNode.h"
+#include "vtkMRMLSelectionNode.h"
 
 
 class stkFiducialMarkerRegistrationWidgetPrivate : public Ui_stkFiducialMarkerRegistrationWidget
@@ -29,6 +33,24 @@ void stkFiducialMarkerRegistrationWidget::on_AddFiducialMarkerToolButton_clicked
 	Q_D(stkFiducialMarkerRegistrationWidget);
 
 	qSlicerApplication * app = qSlicerApplication::application();
-	if (!app)	return;
+	if (!app) return;
 
+	vtkMRMLScene* scene = app->mrmlScene();
+	if (!scene)	return;
+
+	vtkMRMLInteractionNode* interationNode = vtkMRMLInteractionNode::SafeDownCast(scene->GetNthNodeByClass(0, "vtkMRMLInteractionNode"));
+	if(!interationNode)	return;
+
+	vtkMRMLSelectionNode* selectionNode = vtkMRMLSelectionNode::SafeDownCast(scene->GetNthNodeByClass(0, "vtkMRMLSelectionNode"));
+	if (!selectionNode)	return;
+
+	interationNode->SwitchToPersistentPlaceMode();
+
+	if( d->AddFiducialMarkerToolButton->isChecked()){
+		selectionNode->SetReferenceActivePlaceNodeClassName("vtkMRMLMarkupsFiducialNode");
+		interationNode->SetCurrentInteractionMode(vtkMRMLInteractionNode::Place);
+
+	}else{
+		interationNode->SwitchToViewTransformMode();
+	}
 }
