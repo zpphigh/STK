df6a349c8bb44f3c311f403383c9982ac735636f
Weiming Zhai <wmzhai@gmail.com>
2013-10-29 12:25:34 +0800

    allocate image message

--- Libs/SurgeryNavigation/stkIGTLImageClientNode.cpp
+++ Libs/SurgeryNavigation/stkIGTLImageClientNode.cpp
@@ -3,7 +3,7 @@
 
 // OpenIGTLink includes
 #include <igtlServerSocket.h>
-#include <igtlClientSocket.h>
+
 #include <igtlOSUtil.h>
 #include <igtlMessageBase.h>
 #include <igtlMessageHeader.h>
@@ -20,6 +20,19 @@ vtkMRMLNodeNewMacro(stkIGTLImageClientNode);
 //----------------------------------------------------------------------------
 stkIGTLImageClientNode::stkIGTLImageClientNode()
 {
+	imageClientSocket = igtl::ClientSocket::New();
+
+	imageSize[0] = 0;
+	imageSize[1] = 0;
+	imageSize[2] = 0;
+
+	imageSpacing[0] = 0;
+	imageSpacing[1] = 0;
+	imageSpacing[2] = 0;
+
+	imageOrigin[0] = 0;
+	imageOrigin[1] = 0;
+	imageOrigin[2] = 0;
  
 }
 
@@ -29,3 +42,48 @@ stkIGTLImageClientNode::~stkIGTLImageClientNode()
  
 
 }
+
+void stkIGTLImageClientNode::SetImageSize(int x, int y, int z)
+{
+	imageSize[0] = x;
+	imageSize[1] = y;
+	imageSize[2] = z;
+}
+
+void stkIGTLImageClientNode::SetImageSpacing(float x, float y, float z)
+{
+	imageSpacing[0] = x;
+	imageSpacing[1] = y;
+	imageSpacing[2] = z;
+}
+
+void stkIGTLImageClientNode::SetImageOrigin(float x, float y, float z)
+{
+	imageOrigin[0] = x;
+	imageOrigin[1] = y;
+	imageOrigin[2] = z;
+}
+
+void stkIGTLImageClientNode::allocateImageMessage()
+{
+	imageMessage = igtl::ImageMessage::New();
+
+	int   svoffset[] = {0, 0, 0}; // sub-volume offset
+	int   scalarType = igtl::ImageMessage::TYPE_UINT8;// scalar type
+
+	igtl::Matrix4x4 matrix;
+	matrix[0][0] = 1.0;  matrix[1][0] = 0.0;  matrix[2][0] = 0.0; matrix[3][0] = 0.0;
+	matrix[0][1] = 0.0;  matrix[1][1] = 1.0;  matrix[2][1] = 0.0; matrix[3][1] = 0.0;
+	matrix[0][2] = 0.0;  matrix[1][2] = 0.0;  matrix[2][2] = 1.0; matrix[3][2] = 0.0;
+	matrix[0][3] = 0.0;  matrix[1][3] = 0.0;  matrix[2][3] = 0.0; matrix[3][3] = 1.0;
+
+	imageMessage->SetMatrix(matrix);
+	imageMessage->SetDimensions(imageSize);
+	imageMessage->SetSpacing(imageSpacing);
+	imageMessage->SetOrigin(imageOrigin);
+	imageMessage->SetScalarType(scalarType);
+	imageMessage->SetDeviceName("ImageMessage");
+	imageMessage->SetSubVolume(imageSize, svoffset);
+	imageMessage->AllocateScalars();
+}
+
--- Libs/SurgeryNavigation/stkIGTLImageClientNode.h
+++ Libs/SurgeryNavigation/stkIGTLImageClientNode.h
@@ -10,6 +10,8 @@
 // STK includes
 #include "stkSurgeryNavigationExport.h"
 
+#include "igtlClientSocket.h"
+#include "igtlImageMessage.h"
 #include "vtkMRMLNode.h"
 
 class STK_SURGERYNAVIGATION_EXPORT stkIGTLImageClientNode : public vtkMRMLNode
@@ -26,10 +28,25 @@ class STK_SURGERYNAVIGATION_EXPORT stkIGTLImageClientNode : public vtkMRMLNode
     {return "IGTLImageClient";};
 
 
+  void SetImageSize(int x, int y, int z);
+  void SetImageSpacing(float x, float y, float z);
+  void SetImageOrigin(float x, float y, float z);
+
+
  protected:
   stkIGTLImageClientNode();
   ~stkIGTLImageClientNode();
   stkIGTLImageClientNode(const stkIGTLImageClientNode&);
   void operator=(const stkIGTLImageClientNode&);
+
+
+private:
+  igtl::ClientSocket::Pointer imageClientSocket;
+  igtl::ImageMessage::Pointer imageMessage;
+  int imageSize[3];
+  float imageSpacing[3];
+  float imageOrigin[3];
+
+  void allocateImageMessage();
 };
 
