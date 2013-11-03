1471880c66d9f58f356c3599d3fe9eb12ed49fc5
Weiming Zhai <wmzhai@gmail.com>
2013-11-03 20:46:45 +0800

    support 8 layout changes

--- Libs/SlicerExt/stkSlicerLayoutWidget.cpp
+++ Libs/SlicerExt/stkSlicerLayoutWidget.cpp
@@ -1,6 +1,12 @@
 #include "stkSlicerLayoutWidget.h"
 #include "ui_stkSlicerLayoutWidget.h"
 
+//Slicer includes
+#include "qSlicerApplication.h"
+#include "qSlicerLayoutManager.h"
+#include "vtkMRMLLayoutNode.h"
+
+
 class stkSlicerLayoutWidgetPrivate : public Ui_stkSlicerLayoutWidget
 {
 public:
@@ -20,3 +26,84 @@ stkSlicerLayoutWidget::~stkSlicerLayoutWidget()
 {
 
 }
+
+void stkSlicerLayoutWidget::on_SlicerLayoutConventionalViewButton_clicked()
+{
+	qSlicerLayoutManager* layoutManager = qSlicerApplication::application()->layoutManager();
+	if(layoutManager)
+	{
+		layoutManager->setLayout(vtkMRMLLayoutNode::SlicerLayoutConventionalView);
+	}
+}
+
+
+void stkSlicerLayoutWidget::on_SlicerLayoutConventionalWidescreenViewButton_clicked()
+{
+	qSlicerLayoutManager* layoutManager = qSlicerApplication::application()->layoutManager();
+	if(layoutManager)
+	{
+		layoutManager->setLayout(vtkMRMLLayoutNode::SlicerLayoutConventionalWidescreenView);
+	}
+}
+
+
+void stkSlicerLayoutWidget::on_SlicerLayoutFourUpViewButton_clicked()
+{
+	qSlicerLayoutManager* layoutManager = qSlicerApplication::application()->layoutManager();
+	if(layoutManager)
+	{
+		layoutManager->setLayout(vtkMRMLLayoutNode::SlicerLayoutFourUpView);
+	}
+}
+
+
+void stkSlicerLayoutWidget::on_SlicerLayoutOneUp3DViewButton_clicked()
+{
+	qSlicerLayoutManager* layoutManager = qSlicerApplication::application()->layoutManager();
+	if(layoutManager)
+	{
+		layoutManager->setLayout(vtkMRMLLayoutNode::SlicerLayoutOneUp3DView);
+	}
+}
+
+
+void stkSlicerLayoutWidget::on_SlicerLayoutOneUpRedSliceViewButton_clicked()
+{
+	qSlicerLayoutManager* layoutManager = qSlicerApplication::application()->layoutManager();
+	if(layoutManager)
+	{
+		layoutManager->setLayout(vtkMRMLLayoutNode::SlicerLayoutOneUpRedSliceView);
+	}
+}
+
+
+void stkSlicerLayoutWidget::on_SlicerLayoutOneUpYellowSliceViewButton_clicked()
+{
+	qSlicerLayoutManager* layoutManager = qSlicerApplication::application()->layoutManager();
+	if(layoutManager)
+	{
+		layoutManager->setLayout(vtkMRMLLayoutNode::SlicerLayoutOneUpYellowSliceView);
+	}
+}
+
+
+void stkSlicerLayoutWidget::on_SlicerLayoutOneUpGreenSliceViewButton_clicked()
+{
+	qSlicerLayoutManager* layoutManager = qSlicerApplication::application()->layoutManager();
+	if(layoutManager)
+	{
+		layoutManager->setLayout(vtkMRMLLayoutNode::SlicerLayoutOneUpGreenSliceView);
+	}
+}
+
+
+void stkSlicerLayoutWidget::on_SlicerLayoutDual3DViewButton_clicked()
+{
+	qSlicerLayoutManager* layoutManager = qSlicerApplication::application()->layoutManager();
+	if(layoutManager)
+	{
+		layoutManager->setLayout(vtkMRMLLayoutNode::SlicerLayoutDual3DView);
+	}
+}
+
+
--- Libs/SlicerExt/stkSlicerLayoutWidget.h
+++ Libs/SlicerExt/stkSlicerLayoutWidget.h
@@ -23,6 +23,17 @@ public:
 	stkSlicerLayoutWidget(QWidget *parent = 0);
 	~stkSlicerLayoutWidget();
 
+protected slots:
+
+	void on_SlicerLayoutConventionalViewButton_clicked();
+	void on_SlicerLayoutConventionalWidescreenViewButton_clicked();
+	void on_SlicerLayoutFourUpViewButton_clicked();
+	void on_SlicerLayoutOneUp3DViewButton_clicked();
+	void on_SlicerLayoutOneUpRedSliceViewButton_clicked();
+	void on_SlicerLayoutOneUpYellowSliceViewButton_clicked();
+	void on_SlicerLayoutOneUpGreenSliceViewButton_clicked();
+	void on_SlicerLayoutDual3DViewButton_clicked();
+
 protected:
 	QScopedPointer<stkSlicerLayoutWidgetPrivate> d_ptr;
 
