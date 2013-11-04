1dbc31309ec5dd1dba4275b95204fc42a353982c
Weiming Zhai <wmzhai@gmail.com>
2013-11-04 10:29:06 +0800

    launching application

--- Applications/stkAuroraTrackerApp/stkAuroraTrackerAppMain.cpp
+++ Applications/stkAuroraTrackerApp/stkAuroraTrackerAppMain.cpp
@@ -3,11 +3,25 @@
 
 #include "stkAuroraTracker.h"
 #include "stkAuroraTrackerWidget.h"
+#include "qSlicerApplication.h"
+#include "stkSlicerApp.h"
+#include "stkSlicerHelper.h"
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	stkAuroraTrackerWidget w;
-	w.show();
-	return a.exec();
+	QCoreApplication::setOrganizationName("fitark");
+	QCoreApplication::setOrganizationDomain("fitark.org");
+	QCoreApplication::setApplicationName("stkAuroraTrackerApp");
+	QCoreApplication::setApplicationVersion("0.1");
+
+	qSlicerApplication app(argc, argv);
+	if (app.returnCode() != -1)
+		return app.returnCode();
+
+	stkSlicerHelper::SetupModules();	
+	stkSlicerApp* w = new stkSlicerApp;
+	stkSlicerHelper::LoadModules();
+	w->show();
+
+	return app.exec();
 }
