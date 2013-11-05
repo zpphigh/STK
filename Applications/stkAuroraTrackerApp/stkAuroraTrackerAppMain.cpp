c8780c7758611ffae200a2580b490c11f6d146a6
Weiming Zhai <wmzhai@gmail.com>
2013-11-05 17:24:48 +0800

    add stkSlicerLauncherApp

--- Applications/stkAuroraTrackerApp/stkAuroraTrackerAppMain.cpp
+++ Applications/stkAuroraTrackerApp/stkAuroraTrackerAppMain.cpp
@@ -1,27 +1,11 @@
 #include <QtGui>
 #include <QtGui/QApplication>
-
-#include "stkAuroraTracker.h"
 #include "stkAuroraTrackerWidget.h"
-#include "qSlicerApplication.h"
-#include "stkSlicerApp.h"
-#include "stkSlicerHelper.h"
 
 int main(int argc, char *argv[])
 {
-	QCoreApplication::setOrganizationName("fitark");
-	QCoreApplication::setOrganizationDomain("fitark.org");
-	QCoreApplication::setApplicationName("stkAuroraTrackerApp");
-	QCoreApplication::setApplicationVersion("0.1");
-
-	qSlicerApplication app(argc, argv);
-	if (app.returnCode() != -1)
-		return app.returnCode();
-
-	stkSlicerHelper::SetupModules();	
-	stkSlicerApp* w = new stkSlicerApp;
-	stkSlicerHelper::LoadModules();
-	w->show();
-
-	return app.exec();
+	QApplication a(argc, argv);
+	stkAuroraTrackerWidget w;
+	w.show();
+	return a.exec();
 }
